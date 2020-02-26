59596fad06002235bbdf195f703194f32fee0cff
Ajay Birla <ajay.birla@gemsessence.com>
2020-02-26 14:16:21 +0530

    Added active storage

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,5 +1,6 @@
 class Movie < ApplicationRecord
-	has_many :reviews,dependent: :destroy
+  has_many_attached :uploads
+  has_many :reviews,dependent: :destroy
   has_many :favorites,dependent: :destroy
   has_many :fans, through: :favorites, source: :user
 
@@ -10,23 +11,23 @@ class Movie < ApplicationRecord
     validates :duration, presence: true
     validates :description, length: { minimum: 25 }
     validates :total_gross, :numericality => { :greater_than_or_equal_to => 0 }
-    validates :image_file_name, allow_blank: true,format:{
-        with: /\w+\.(gif|jpg|png)\z/i,
-        message: "must reference a GIF, JPG, or PNG "
-    }
+    # validates :image_file_name, allow_blank: true,format:{
+    #     with: /\w+\.(gif|jpg|png)\z/i,
+    #     message: "must reference a GIF, JPG, or PNG "
+    # }
     RATINGS = %w(G PG PG-13 R NC-17)
-	def flop?
-		total_gross<50000000
-	end
-	def self.released
-		where("released_on <?",Time.now).order(released_on: :desc)
-	end
-		
+  def flop?
+    total_gross<50000000
+  end
+  def self.released
+    where("released_on <?",Time.now).order(released_on: :desc)
+  end
 
-   def average_stars
-        reviews.average(:stars)            
-   end
-   def self.recent_reviews(movie)
-       movie.reviews.order(updated_at: :desc).limit(2)
-   end
+  def average_stars
+    reviews.average(:stars)
+  end
+
+  def self.recent_reviews(movie)
+    movie.reviews.order(updated_at: :desc).limit(2)
+  end
 end
